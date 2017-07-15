3b1ea1c091393315dbe1c321a8652018d88e0e99
Michael Evans <azmathtech@gmail.com>
2017-07-15 11:05:28 -0700

    Init commit

---
+++ app/components/Navigation.jsx
@@ -0,0 +1,33 @@
+var React = require('react');
+var {Link, IndexLink} = require('react-router');
+
+var Navigation = React.createClass({
+
+  render: function () {
+    return (
+      <div className="top-bar">
+        <div className="top-bar-left">
+          <ul className="menu">
+            <li className="menu-text">Boilerplate App</li>
+            <li>
+              <IndexLink to="/" activeClassName="active" activeStyle={{fontWeight: 'bold'}} >First Link</IndexLink>
+            </li>
+            <li>
+              <IndexLink to="/SecondLink" activeClassName="active" activeStyle={{fontWeight: 'bold'}} >SecondLink</IndexLink>
+            </li>
+          </ul>
+        </div>
+        <div className="top-bar-right">
+          <ul className="menu">
+            <li className="menu-text">
+              Designed by <a href="http://www.recoverycoffees.com" target="_blank">Michael Evans</a>
+            </li>
+          </ul>
+        </div>
+      </div>
+    );
+  }
+
+});
+
+module.exports = Navigation;
---
+++ webpack.config.js
@@ -0,0 +1,60 @@
+var webpack = require('webpack');
+var path = require('path');
+
+module.exports = {
+  //entry: './app/app.jsx',
+  entry: [
+    'script!jquery/dist/jquery.min.js',
+    'script!foundation-sites/dist/js/foundation.min.js',
+    './app/app.jsx'
+  ],
+  externals: {
+    jquery: 'jQuery'
+  },
+  plugin: [
+    new webpack.ProvidePlugin({
+      '$': 'jquery',
+      'jQuery': 'jquery'
+    })
+  ],
+  output: {
+    path: __dirname,
+    filename: './public/bundle.js'
+  },
+  resolve: {
+    root: __dirname,
+    alias: {
+      Main: 'app/components/Main.jsx',
+      Navigation: 'app/components/Navigation.jsx',
+      applicationStyles: 'app/styles/app.scss'
+      //applicationStyles: 'app/styles/app.css'
+    },
+    extensions: ['', '.js', '.jsx']
+  },
+  module: {
+    loaders: [
+      {
+        loader: 'babel-loader',
+        query: {
+          presets: ['react', 'es2015']
+        },
+        test: /\.jsx?$/,
+        exclude: /(node_modules|bower_components)/
+      }
+    ]
+  },
+  sassLoader: {
+    includePaths: [
+      path.resolve(__dirname, './node_modules/foundation-sites/scss')
+    ]
+  },
+  devtool: 'cheap-module-eval-source-map'
+};
+
+
+
+// set the value to "cheap-module-eval-source-map".
+//This might cause the source maps to not work as shown in the video.
+//
+// If you are getting this error, try setting the value to either
+//"inline-source-map" or "eval-source-map" instead
