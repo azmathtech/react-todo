e9cc720e9c948c6f72b28f2307e78e201318ae50
Michael Evans <azmathtech@gmail.com>
2017-07-18 11:51:11 -0700

    Add filter functionality

--- app/api/TodoAPI.jsx
+++ app/api/TodoAPI.jsx
@@ -19,9 +19,41 @@ module.exports = {
     }
 
     return $.isArray(todos) ? todos : [];
-    
+  },
+
+  filterTodos: function(todos, showCompleted, searchText) {
+    var filteredTodos = todos;
+
+    //Filter by showCompleted
+    filteredTodos = filteredTodos.filter((todo) => {
+      return !todo.completed || showCompleted;
+    });
+
+    //Filter by searchText
+    filteredTodos = filteredTodos.filter((todo) => {
+      var text = todo.text.toLowerCase();
+
+      return searchText.length === 0 || text.indexOf(searchText) > -1;
+    });
+
+    //Sort todos with non-completed first
+    //if you return -1 then a should come before b
+    //if you return 1 then b should come before a
+    //if you return 0 then no change a and b are equal
+    filteredTodos.sort((a, b) => {
+      if (!a.completed && b.completed) {
+        return -1;
+      } else if (a.completed && !b.completed) {
+        return 1;
+      } else {
+        return 0;
+      }
+    });
+
+    return filteredTodos;
   }
-}
+
+};
 
 // // the two statements below do the same thing
 // // it is two methods of writing an if statement
@@ -32,3 +64,8 @@ module.exports = {
 // } else {
 //   return [];
 // }
+
+
+// //if statements - the below are the same
+// if (a.completed === false && b.completed === true) {}
+// if (!a.completed && b.completed) {}
--- app/components/TodoApp.jsx
+++ app/components/TodoApp.jsx
@@ -55,7 +55,8 @@ var TodoApp = React.createClass({
   },
 
   render: function () {
-    var {todos} = this.state;
+    var {todos, showCompleted, searchText} = this.state;
+    var filteredTodos = TodoAPI.filterTodos(todos, showCompleted, searchText);
 
     return (
       <div>
@@ -63,7 +64,7 @@ var TodoApp = React.createClass({
         <div className="row">
           <div className="column small-centered medium-6 large-4">
             <TodoSearch onSearch={this.handleSearch}/>
-            <TodoList todos={todos} onToggle={this.handleToggle}/>
+            <TodoList todos={filteredTodos} onToggle={this.handleToggle}/>
             <AddTodo onAddTodo={this.handleAddTodo}/>
           </div>
         </div>
