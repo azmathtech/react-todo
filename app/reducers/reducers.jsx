4ca06c5911aabff3854a641f7a335acc86cacedf
Michael Evans <azmathtech@gmail.com>
2017-07-20 20:50:06 -0700

    Fixed searching with capitals issue

--- app/reducers/reducers.jsx
+++ app/reducers/reducers.jsx
@@ -4,7 +4,7 @@ var moment = require('moment');
 export var searchTextReducer = (state = '', action) => {
   switch (action.type) {
     case 'SET_SEARCH_TEXT':
-      return action.searchText;
+      return action.searchText.toLowerCase();
     default:
       return state;
   };
